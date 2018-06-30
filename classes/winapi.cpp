6fb9bca2ad5693ccfea64f5d2a7b968f64571ae4
Ouspheaktra <ouspheaktraphal@gmail.com>
2018-06-30 13:12:52 +0700

    modify winapi

--- classes/winapi.cpp
+++ classes/winapi.cpp
@@ -1,13 +1,9 @@
 #include "winapi.h"
 #include <windows.h>
-
-
-void Draw::SetWindow(HWND hwnd) {
-	this->hwnd = hwnd;
+Draw::Draw() {
 	width = 1;
 	color = RGB(0, 0, 0);
 	backgroundColor = RGB(255, 255, 255);
-	Prepare();
 }
 void Draw::SetColor(COLORREF color) {
 	this->color = color;
@@ -26,31 +22,42 @@ HDC Draw::GetHDC() {
 	return hdc;
 }
 
-void Draw::Prepare() {
-//		hdc = BeginPaint(hwnd, &ps);
-	hdc = GetDC(hwnd);
+void Draw::Begin(HWND hwnd) {
+	return Begin(GetDC(hwnd));
+}
+void Draw::Begin(HDC hdc) {
+	this->hdc = hdc;
 	SetColor(color);
 	SetBackgroundColor(backgroundColor);
 }
-void Draw::Finish() {
-	//EndPaint(hwnd, &ps);
-	ReleaseDC(hwnd, hdc);
+void Draw::End() {
+	ReleaseDC(WindowFromDC(hdc), hdc);
 }
 
 POINT Draw::GetCenter() {
 	RECT rect;
-	GetClientRect(hwnd, &rect);
-	return (POINT){rect.right/2, rect.bottom/2};
+	GetClientRect(WindowFromDC(hdc), &rect);
+	POINT p;
+	p.x = rect.right/2;
+	p.y = rect.bottom/2;
+	return p;
 }
 
 void Draw::Line(int x1, int y1, int x2, int y2) {
-	Prepare();
 	MoveToEx(hdc, x1, y1, NULL);
 	LineTo(hdc, x2, y2);
-	Finish();
 }
 void Draw::Circle(int x, int y, int radius) {
-	Prepare();
 	Ellipse(hdc, x-radius, y-radius, x+radius, y+radius);
-	Finish();
+}
+void Draw::Image(int RESOURCE_ID, int x, int y) {
+	HBITMAP hBitmap = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(RESOURCE_ID));
+	BITMAP bitmap;
+	GetObject(hBitmap, sizeof(bitmap), &bitmap);
+
+	HDC hdcBitmap = CreateCompatibleDC(hdc);
+	SelectObject(hdcBitmap, hBitmap);
+	BitBlt(hdc, x, y, bitmap.bmWidth, bitmap.bmHeight, hdcBitmap, 0, 0, SRCCOPY);
+	DeleteDC(hdcBitmap);
+	DeleteObject(hBitmap);
 }
--- classes/winapi.h
+++ classes/winapi.h
@@ -1,29 +1,29 @@
 #ifndef WINAPI_H
 #define WINAPI_H
 
-#include <windef.h>
+#include <windows.h>
 
 class Draw {
-	HWND hwnd;
 	HDC hdc;
-	//PAINTSTRUCT ps;
 	COLORREF color, backgroundColor;
 	int width;
 public:
-	void SetWindow(HWND hwnd);
+	Draw();
 	void SetColor(COLORREF color);
 	void SetBackgroundColor(COLORREF color);
 	void SetWidth(int width);
 	
 	HDC GetHDC();
 
-	void Prepare();
-	void Finish();
+	void Begin(HWND hwnd);
+	void Begin(HDC hdc);
+	void End();
 	
 	POINT GetCenter();
 	
 	void Line(int x1, int y1, int x2, int y2);
 	void Circle(int x, int y, int radius);
+	void Image(int RESOURCE_ID, int x, int y);
 };
 
 #endif
